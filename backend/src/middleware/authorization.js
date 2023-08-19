54db7b5a268e4629f960993f90929628cc605760
Nitin <ntnbhat9@gmail.com>
2023-08-19 01:35:29 -0400

    fixed backend

---
+++ backend/src/middleware/authorization.js
@@ -0,0 +1,33 @@
+import "express";
+
+export const isAuthenticated = async (req, res, next) => {
+    if (req.session.currentUser === null || req.session.currentUser === undefined) {
+        res.status(400).send("Unauthorized!");
+        return;
+    }
+    next();
+}
+
+export const isAuthenticatedExpert = async (req, res, next) => {
+    if (req.session.currentUser === null || req.session.currentUser === undefined) {
+        res.status(400).send("Unauthorized!");
+        return;
+    }
+    if(req.session.currentUser.role !== 'Expert'){
+        res.status(400).send("Unauthorized! Not an expert");
+        return;
+    }
+    next();
+}
+
+export const isAuthenticatedAdmin = async (req, res, next) => {
+    if (req.session.currentUser === null || req.session.currentUser === undefined) {
+        res.status(400).send("Unauthorized!");
+        return;
+    }
+    if(req.session.currentUser.role !== 'Admin'){
+        res.status(400).send("Unauthorized! Not an Admin");
+        return;
+    }
+    next();
+}
--- backend/src/routes/following.js
+++ backend/src/routes/following.js
@@ -1,9 +1,20 @@
 import express from "express";
+import UserFollowingExpertModel from "../models/UserFollowingExpert.js";
+import UserFollowingTopicModel from "../models/UserFollowingTopic.js";
+import {isAuthenticated} from "../middleware/authorization.js";
 
 const router = express.Router();
 
-router.get("/experts", async (req, res) => {});
+router.get('/experts', isAuthenticated, async (req, res) => {
+    const user = req.query.user?.toString() || "";
+    const followedExperts = await UserFollowingExpertModel.readFollowedExperts(user) || [];
+    res.status(200).send(followedExperts);
+});
 
-router.get("/topics", async (req, res) => {});
+router.get('/topics', isAuthenticated, async (req, res) => {
+    const user = req.query.user?.toString() || "";
+    const followedTopics = await UserFollowingTopicModel.readFollowedTopics(user) || [];
+    res.status(200).send(followedTopics);
+});
 
 export default router;

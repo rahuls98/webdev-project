50560bb8d9f0842e8b2fd722e3815beb746a62a2
Rahul Suresh <rahs98@gmail.com>
2023-08-20 16:07:23 -0400

    Refactored delete

--- backend/src/routes/post.js
+++ backend/src/routes/post.js
@@ -1,6 +1,9 @@
 import express from "express";
 import PostModel from "../models/Post.js";
-import {isAuthenticated, isAuthenticatedAndOwnerPost} from "../middleware/authorization.js";
+import {
+    isAuthenticated,
+    isAuthenticatedAndOwnerPost,
+} from "../middleware/authorization.js";
 
 const router = express.Router();
 
@@ -40,14 +43,18 @@ router.put("/upvote", isAuthenticated, async (req, res) => {
     });
 });
 
-router.delete("/upvote", isAuthenticated, async (req, res) => {
-    const post = req.body.post;
-    const expert = req.body.expert;
-    await PostModel.removePostUpvote(post, expert);
-    res.status(200).send({
-        msg: "Success!",
-    });
-});
+router.delete(
+    "/upvote/:postId/:expertId",
+    isAuthenticated,
+    async (req, res) => {
+        const post = req.params.postId;
+        const expert = req.params.expertId;
+        await PostModel.removePostUpvote(post, expert);
+        res.status(200).send({
+            msg: "Success!",
+        });
+    }
+);
 
 router.put("/downvote", isAuthenticated, async (req, res) => {
     const post = req.body.post;
@@ -58,16 +65,18 @@ router.put("/downvote", isAuthenticated, async (req, res) => {
     });
 });
 
-router.delete("/downvote/:postId", isAuthenticated, async (req, res) => {
-    // const post = req.body.post;
-    const post = req.params.postId;
-    console.log(post);
-    const expert = req.body.expert;
-    await PostModel.removePostDownvote(post, expert);
-    res.status(200).send({
-        msg: "Success!",
-    });
-});
+router.delete(
+    "/downvote/:postId/:expertId",
+    isAuthenticated,
+    async (req, res) => {
+        const post = req.params.postId;
+        const expert = req.params.expertId;
+        await PostModel.removePostDownvote(post, expert);
+        res.status(200).send({
+            msg: "Success!",
+        });
+    }
+);
 
 router.get("/saved", isAuthenticated, async (req, res) => {
     const user = req.query.user?.toString() || "";
@@ -93,16 +102,16 @@ router.put("/unsave", isAuthenticated, async (req, res) => {
     });
 });
 
-router.delete('/delete', isAuthenticatedAndOwnerPost, async (req, res) => {
+router.delete("/delete", isAuthenticatedAndOwnerPost, async (req, res) => {
     try {
         const postId = req.query.postId;
         await PostModel.deletePostById(postId);
         res.status(200).send({
-            "msg": "Success!"
-        })
+            msg: "Success!",
+        });
     } catch (error) {
         console.log(error);
     }
-})
+});
 
 export default router;
